1634d92ce4ecf7443a0c7fa98919ee3af7d6d7f3
Yurtin Alexey <33059547+Lideor@users.noreply.github.com>
2024-09-25 13:36:05 +0500

    add standart normalize

--- DataProducers/Normalizers.py
+++ DataProducers/Normalizers.py
@@ -2,7 +2,7 @@ from abc import abstractmethod, ABC
 from dataclasses import dataclass
 
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
 @dataclass
@@ -24,7 +24,7 @@ class Normalizer(ABC):
 @dataclass
 class MinMaxNormalizer(Normalizer):
     feature_range: tuple = (0, 1)
-    scaler: MinMaxScaler = None
+    scaler = None
 
     def fit(self, X: np.ndarray) -> np.ndarray:
         self.scaler = MinMaxScaler(feature_range=self.feature_range)
@@ -40,3 +40,10 @@ class MinMaxNormalizer(Normalizer):
         if self.scaler is None:
             raise ValueError("The normalizer has not been fitted yet. Call 'fit' first.")
         return self.scaler.inverse_transform(X)
+
+
+
+@dataclass
+class StandardNormalizer(MinMaxNormalizer):
+    def fit(self, X: np.ndarray) -> np.ndarray:
+        self.scaler = StandardScaler()
